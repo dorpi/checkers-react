29cb36c24eb0187ce42544428f5dd243b8fead5e
dorpi <dorpi@users.noreply.github.com>
2020-09-17 18:22:37 +0300

    Update GameDisplay.js

--- src/components/GameDisplay.js
+++ src/components/GameDisplay.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import PropsTypes from 'prop-types';
-
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import ScoreComponent from './ScoreComponent';
-
 
 import CheckersBoardGame from './board/CheckersBoardGame';
 import './display-style.css'
@@ -19,25 +14,17 @@ class Game extends Component {
         const { score, moves } = this.props
         return (
 
-
-            <Container className="game-container">
-                <Row>
-                    <Col xs={3}>
-                        <ScoreComponent score={score} moves={moves} />
-                    </Col>
-                    <Col >
-                        <h4 className='text-center title'>Checkers Board Game</h4>
-                    </Col>
-                </Row>
-                <Row>
+            <div className="game-container">
+                <div className="headline">
+                    <h1 className='headline-text'>Checkers</h1>
+                    <ScoreComponent score={score} moves={moves} />
+                </div>
                     <div className='divider'></div>
-                </Row>
-                <Row className='row justify-content-md-center'>
-                    <Col >
+              
+                   
                         <CheckersBoardGame />
-                    </Col>
-                </Row>
-            </Container>
+
+            </div>
         )
     }
 }
